76f2a0d447bc589302eb5b19663e6b5f81b3c4d2
DarthBeta <Sean.Pavlichek@gmail.com>
2015-09-23 21:06:37 -0400

    Updated jump code
    Modified jump to be more efficient, more smooth, and take less code.
    Also modified camera to follow the player.

--- Project1/Prototype1/Assets/Jump.cs
+++ Project1/Prototype1/Assets/Jump.cs
@@ -10,11 +10,21 @@ public class Jump : MonoBehaviour {
 	int rotations;
 	bool falling;
 	Vector2 start;
-	
 	private Collider2D floorCheck;// = transform.gameObject;
 	public bool grounded = false;
+	private GameObject player;
+	private moveLeftOrRight movement;
 	//private bool jumping = false;
-	
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		falling = false;
+		rotations = 8;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		movement = player.GetComponent<moveLeftOrRight> ();
+	}
+
 	//Called at initialization of game
 	public void land(){
 		//Debug.Log("landing");
@@ -28,18 +38,11 @@ public class Jump : MonoBehaviour {
 		grounded = false;
 		falling = false;
 	}
-	
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D>();
-		falling = false;
-		rotations = 8;
-	}
-	
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
 		//Debug.Log ("keycode: "+Input.GetKeyDown(KeyCode.Space)+" grounded: "+grounded);
 		//jumping = false;
 		
@@ -54,6 +57,31 @@ public class Jump : MonoBehaviour {
 			//jumping = true;
 			
 		}
+
+		// variable to store the player's direction
+		int leftOrRight;
+		// gets the player's direction
+		if (movement.dir == 1) {
+			leftOrRight = -1;
+		} else {
+			leftOrRight = 1;
+		}
+		// if the player has fully rotated, resets the rotation
+		if (rotations >= 24) {
+			rotations = 0;
+		}
+		// rotates the player
+		if (!grounded && (rotations % 2 == 0)) {
+			rb.transform.Rotate (0, 0, (30*leftOrRight)); // rotates 60 degrees
+			rotations++; // marks the rotation the player is on
+		} else if (!grounded && (rotations % 2 == 1)) { // added to slow the rotation down, the rotation only happens on evens
+			rotations++;
+		} else if (rotations > 0) { // if the player has landed, reset their rotation
+			rotations = 0;
+			rb.transform.rotation = new Quaternion(0, 0, 0, 0);
+		}
+
+		/*
 		// Causes the player to rotate during the jump
 		if ((rb.position.y > ((.25 * (jumpStrength / 3)) + start.y)) && !falling && rotations < 1) {// first part of the jump
 			rb.transform.Rotate (0, 0, 60); // rotates 60 degrees
@@ -86,7 +114,8 @@ public class Jump : MonoBehaviour {
 		} else if (grounded) {// if the player is done jumping, reset their rotation
 			rb.transform.rotation = new Quaternion(0, 0, 0, 0);
 		}
-		
+		*/
+
 		if(rb.velocity.y < -maxFallSpeed){
 			rb.velocity = new Vector2(rb.velocity.x, -maxFallSpeed);
 		}
--- Project1/Prototype1/Assets/moveLeftOrRight.cs
+++ Project1/Prototype1/Assets/moveLeftOrRight.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class moveLeftOrRight : MonoBehaviour {
 
 	public float moveSpeed;
+	public int dir;
 
 	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		dir = 1;
 	}
 	
 	// Update is called once per frame
@@ -19,10 +21,12 @@ public class moveLeftOrRight : MonoBehaviour {
 
 		if(Input.GetKey(KeyCode.RightArrow)){
 			rb.velocity += new Vector2(moveSpeed, 0);
+			dir = 1;
 		}
 
 		if(Input.GetKey(KeyCode.LeftArrow)){
 			rb.velocity += new Vector2(-moveSpeed, 0);
+			dir = 0;
 		}
 
 	}
