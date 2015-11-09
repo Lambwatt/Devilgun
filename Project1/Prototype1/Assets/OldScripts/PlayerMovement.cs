2c2dc7d5489c5ea8310dca5af4c1a52ec778800b
Michael Pettitt <mikerpettitt@gmail.com>
2015-11-09 16:27:03 -0500

    Start of playermovent
    Started the playermovement script.

---
+++ Project1/Prototype1/Assets/OldScripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1;
+
+	private Vector3 horizontalDirection;
+	private Rigidbody move;
+
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Rigidbody> ();
+
+		horizontalDirection = Vector3.right;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
+
+		move.velocity = new Vector3((h * speed), 0, 0);
+
+
+
+
+	}
+}
