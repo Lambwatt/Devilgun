1130b4e6b06125118a0c8aaf2ba5d2205623c786
Michael Pettitt <mikerpettitt@gmail.com>
2015-11-10 20:37:18 -0500

    Some updates to player movement.
    Added run/walk and jump. Need to add a bit more still

--- Project1/Prototype1/Assets/OldScripts/PlayerMovement.cs
+++ Project1/Prototype1/Assets/OldScripts/PlayerMovement.cs
@@ -4,9 +4,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed = 1;
+	public float walkSpeed = 4f;
+	public float runSpeed = 8f;
+	public float jumpForce = 20f;
 
-	private Vector3 horizontalDirection;
+	private bool walk = true;
+	private bool grounded = true;
 	private Rigidbody move;
 
 
@@ -14,18 +17,66 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
 		move = GetComponent<Rigidbody> ();
 
-		horizontalDirection = Vector3.right;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
 
-		move.velocity = new Vector3((h * speed), 0, 0);
-
+		ToggleRun ();
 
 
+		if (walk) {
+			Walk();
+		} else if (!walk) {
+			Run();
+		}
+
+		Jump ();
 
 	}
+
+	void FixedUpdate(){
+/*
+		ToggleRun ();
+		
+		
+		if (walk) {
+			Walk();
+		} else if (!walk) {
+			Run();
+		}
+		
+		Jump ();
+*/
+	}
+
+	void ToggleRun(){
+		if (CrossPlatformInputManager.GetButtonDown ("Caps") && walk) {
+			walk = false;
+		} else if (CrossPlatformInputManager.GetButtonDown ("Caps") && !walk) {
+			walk = true;
+		}
+	}
+
+	void Walk(){
+		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
+		move.velocity = new Vector3((h * walkSpeed), 0, 0);
+	}
+
+	void Run(){
+		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
+		move.velocity = new Vector3((h * runSpeed), 0, 0);
+	}
+
+	void Jump(){
+		Vector3 up = new Vector3 (0, 1, 0);
+
+		if (CrossPlatformInputManager.GetButton ("Jump")) {
+			move.AddForce((up *jumpForce) , ForceMode.Impulse);
+		}
+
+	}
+
+
 }
